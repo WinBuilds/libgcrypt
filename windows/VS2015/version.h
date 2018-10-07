b9514afe7ae648de386b64d45e71e339bb7cbccf
latsa <laca@gmx.us>
2018-10-07 14:40:34 +0300

    -

---
+++ windows/VS2015/version.h
@@ -0,0 +1,11 @@
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.8.3"
+
+/* Version of this package */
+#define VERSION "1.8.3"
+
+/* GIT commit id revision used to build this package */
+#define BUILD_REVISION "5600d2d"
+
+/* The time this package was configured for a build */
+#define BUILD_TIMESTAMP "2018-06-17T12:43+0000"
